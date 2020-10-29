bad369f9f3c6f3386f63f8041d4843447363463e
s-pc <s-pc@localhost>
2020-10-30 00:43:21 +0530

    Thymeleaf web project committed

---
+++ StudentCrmWithJPARepoAndThymeleafWeb/src/main/java/com/studentcrm/StudentCrmWithJpaRepoAndJspWebApplication.java
@@ -0,0 +1,13 @@
+package com.studentcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrmWithJpaRepoAndJspWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrmWithJpaRepoAndJspWebApplication.class, args);
+	}
+
+}
